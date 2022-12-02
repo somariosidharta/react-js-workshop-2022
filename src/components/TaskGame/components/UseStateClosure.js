cd803354e2e0205c8112ba68f6a01f92876a86f9
somariosidharta <49771935+somariosidharta@users.noreply.github.com>
2022-12-02 15:37:31 +0700

    fix

--- src/components/TaskGame/components/UseStateClosure.js
+++ src/components/TaskGame/components/UseStateClosure.js
@@ -10,10 +10,8 @@ function UseStateClosure(props) {
 
   const handleClickAsync = () => {
     console.log("handlernya sudah onclick");
-    setTimeout(() => {
       // TODO: Find the Bugs
       setCount(count + 1);
-    }, 1000);
   };
 
   return (
